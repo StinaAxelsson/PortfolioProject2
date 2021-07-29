71622486a363d43227bb22854fb906dcdc8073da
StinaAxelsson <stina91@live.se>
2021-07-29 20:34:28 +0000

    add comments on the function in script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
 
 
-
+ //Variables
 const player ={
     top: 350,
     left: 130
 };
 
 const bullets = [];
-const comets = [
-    { left: 100, top: 0 },
-    { left: 150, top: 0 },
-    { left: 200, top: 0 },
-];
+const gameArea = document.getElementById('gameArea');
 
+/**
+ * Keyboard functions. When press arrow left and right the player will move, 
+ * and press space to fire bullets
+ * */  
 document.onkeydown = function(e){
 
     if (e.keyCode === 37 ){
@@ -37,25 +37,34 @@ document.onkeydown = function(e){
     }
 
 
-
+/**
+ * Place the player on the gameArea
+ */
 function drawPlayer() {
     document.getElementById('player').style.left = player.left + 'px';
     document.getElementById('player').style.top = player.top + 'px';
 }
-
+/**
+ * Make the bullets and place them with the player
+ */
 function drawBullets() {
     document.getElementById('bullets').innerHTML = ""
     for(let i = 0 ; i < bullets.length ; i++ ) {
         document.getElementById('bullets').innerHTML += `<div class='bullet' style='left:${bullets[i].left}px; top:${bullets[i].top}px'></div>`;
     }
 }
-
+/**
+ * make the bullet follow the players movement in the gameArea
+ */
 function moveBullets() {
     for(let i = 0 ; i < bullets.length ; i++ ) {
          bullets[i].top = bullets[i].top - 20
     }
 }
 
+/**
+ * Make new comets and place them in random places on top of the gameArea
+ */
 function drawComets(){ 
 setInterval(() => {
     let comets = document.createElement("div");
@@ -66,36 +75,48 @@ setInterval(() => {
         comets.style.left = Math.floor(Math.random() * 250) + "px";
 
         gameArea.appendChild(comets);
-}, 500); {
+}, 1000); {
         
     }
 }
+/**
+ * Make the comets move from the top of the gameArea 
+ * and when touches the bottom an alert stop the game 
+ * and say gameover.
+ */
+function moveComets() { 
+setInterval(() => {
 
-function moveComets() {
-    for(var i = 0 ; i < comets.length ; i++ ) {
-        comets[i].top = comets[i].top + 5;
+     comet = document.getElementsByClassName('comet');
+
+    if (comet != undefined){
+        for (let i=0; i < comet.length; i++){
+            let comets = comet[i];
+            let comettop = parseInt(
+                window.getComputedStyle(comets).getPropertyValue("top")
+              );
+              if (comettop >= 350) {
+                alert("Game Over");
+                clearInterval(moveComets);
+                window.location.reload();
+              }
+
+              comets.style.top = comettop + 25 + "px";
+        }
     }
+    
+}, 450); {
+
+}
 }
 
 function collisionDetection() {
-    for (var comet = 0; comet < comets.length; comet++) {
-        for (var bullet = 0; bullet < bullets.length; bullet++) {
-            if ( 
-                bullets[bullet].left >= comets[comet].left  &&
-                bullets[bullet].left <= (comets[comet].left + 50)  &&
-                bullets[bullet].top <= (comets[comet].top + 50)  &&
-                bullets[bullet].top >= comets[comet].top
-            ) {
-                comets.splice(comet, 1);
-                bullets.splice(bullet, 1);
-            }
-        }
-    }
+
 }
 
 function gameLoop(){
 
-    setTimeout(gameLoop, 100)
+    setTimeout(gameLoop, 1000)
     moveBullets();
     drawBullets();
     moveComets();
